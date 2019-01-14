52e502e8e570716e263e6941c323b06326caabe5
Steve Archuleta <stevearchuleta@me.com>
2019-01-13 21:26:01 -0800

    last commmit; I'm really sick with the flu.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,7 +21,6 @@ for(var i=0; i < 4; i++){
     crystal.attr({
       "class": 'crystal',
       'data-random': random
-      
     });
 
     crystal.html(random);
@@ -39,14 +38,21 @@ resetAndStart();
   var num = parseInt($(this).attr('data-random'));
 
   previous += num;
-  console.log(previous)
+
+  $("previous").html(previous);
+  //console.log(previous)
 
   if (previous > randomResult){
     losses--;
+    
     $('#losses').html(losses);
-  //console.log("You lost!");
-  previous = 0;
+    //console.log("You lost!");
+    previous = 0;
+    
+    $("previous").html(previous);
+    
     resetAndStart();
+    
   }
   else if (previous === randomResult){
     wins++;
@@ -54,9 +60,11 @@ resetAndStart();
     $('#wins').html(wins);
 
     previous = 0;
-
+    
+    $("previous").html(previous);
     resetAndStart();
+    
     //console.log ('You win!');
   }
-  //console.log (preceding);
+    //console.log (preceding);
 });     
